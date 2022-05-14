fbce0eb4bac141b16d5a8cfc9bca72a156d97a0d
Guilherme Souza <grsouza@pm.me>
2022-05-14 08:26:48 -0300

    Implement unit test

--- Tests/AnalyticsClientTests/AnalyticsClientTests.swift
+++ Tests/AnalyticsClientTests/AnalyticsClientTests.swift
@@ -1,11 +1,26 @@
 import XCTest
+
 @testable import AnalyticsClient
 
 final class AnalyticsClientTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(AnalyticsClient().text, "Hello, World!")
+  func testDefaultClient() throws {
+    let event = AnalyticsEvent(name: "test_run")
+
+    var destination1Called = false
+    let destination1 = AnalyticsDestination {
+      XCTAssertEqual(event, $0)
+      destination1Called = true
     }
+
+    var destination2Called = false
+    let destination2 = AnalyticsDestination {
+      XCTAssertEqual(event, $0)
+      destination2Called = true
+    }
+
+    let client = AnalyticsClient.default([destination1, destination2])
+    client.track(event)
+
+    XCTAssertEqual([destination1Called, destination2Called], [true, true])
+  }
 }

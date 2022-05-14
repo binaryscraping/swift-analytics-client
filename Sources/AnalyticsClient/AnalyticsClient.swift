97e64675a0b343b304578dfb960339cbdda96d60
Guilherme Souza <grsouza@pm.me>
2022-05-14 08:23:26 -0300

    Add AnalyticsClient type

--- Sources/AnalyticsClient/AnalyticsClient.swift
+++ Sources/AnalyticsClient/AnalyticsClient.swift
@@ -1,6 +1,49 @@
-public struct AnalyticsClient {
-    public private(set) var text = "Hello, World!"
+public struct AnalyticsEvent: Hashable, Codable, Identifiable {
+  public var id: String { name }
 
-    public init() {
+  public var name: String
+  public var properites: [String: String]?
+
+  public init(name: String, properties: [String: String]? = nil) {
+    self.name = name
+    self.properites = properties
+  }
+}
+
+public struct AnalyticsClient {
+  public var track: (_ event: AnalyticsEvent) -> Void
+
+  public init(track: @escaping (_ event: AnalyticsEvent) -> Void) {
+    self.track = track
+  }
+
+  /// The default client implementation that sends the event to multiple provided destinations.
+  /// - Parameter destinations: Destinations to send the event to.
+  /// - Returns: The default analytics client.
+  public static func `default`(_ destinations: [AnalyticsDestination]) -> AnalyticsClient {
+    AnalyticsClient { event in
+      for destination in destinations {
+        destination.send(event)
+      }
     }
+  }
+}
+
+/// A Destination where ``AnalyticsEvent`` can be sent to, for example, console, file, remote database, proprietary services like Crashlytics.
+public struct AnalyticsDestination {
+  public var send: (_ event: AnalyticsEvent) -> Void
+
+  public init(send: @escaping (_ event: AnalyticsEvent) -> Void) {
+    self.send = send
+  }
+}
+
+extension AnalyticsDestination {
+  public static var console: AnalyticsDestination {
+    AnalyticsDestination { event in
+      #if DEBUG
+        print("[Analytics] Tracked event \(event.name) with properties \(event.properites ?? [:])")
+      #endif
+    }
+  }
 }
